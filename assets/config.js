0bf9d49ee4c7ecc282c17c497cc8d2b524562097
quicktest007 <jd.hcovenanteyes@gmail.com>
2026-02-12 14:31:36 -0500

    Update WEBHOOK_URL in config.js to include a Google Apps Script Web App URL; add example comment for clarity on usage.

--- assets/config.js
+++ assets/config.js
@@ -1,5 +1,6 @@
 /**
  * Optional: set WEBHOOK_URL to POST leads to your endpoint.
  * Leave empty to only store leads in localStorage.
+ * Example: Google Apps Script Web App URL (see README or project docs).
  */
-var WEBHOOK_URL = "";
+var WEBHOOK_URL = "https://script.google.com/macros/s/AKfycbxgBgwHzhxqmT6LETD-BDwWHk4bPl0wpz5K6zQxKANMB_dcH-cRvrZ8plPwB3U-Xw-fbg/exec";
